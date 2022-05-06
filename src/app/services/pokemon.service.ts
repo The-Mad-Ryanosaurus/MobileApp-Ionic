9a292dd19d0a428ae5eb4a4c00899db11cac2a4e
Ryan Harte <ryanharte1997@gmail.com>
2022-05-06 14:23:02 +0100

    Api search functionality implemented

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -6,10 +6,10 @@ import { map } from 'rxjs/operators'
   providedIn: 'root'
 })
 export class PokemonService {
-
+  //base url for the api and the image url for the images associated to each pokemon in the api
   baseUrl = 'https://pokeapi.co/api/v2';
   imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-
+  //used to map/display the api and all its contents/pokemon along with the images
   constructor(private http:HttpClient) { 
   }
   getPokemon(offset = 0){
@@ -26,8 +26,32 @@ export class PokemonService {
     })
     )
   }
-
+  //gets the images matched to the pokemon
   getPokeImage(index){
     return `${this.imageUrl}${index}.png`;
+  }
+  //service for finding a pokemon by name/id from searchbar by use of pip/maps
+  //search only works if full name is put in - eg "Bulbasaur"
+  findPokemon(search){
+    return this.http.get(`${this.baseUrl}/pokemon/${search}`).pipe(
+      map(pokemon => {
+        pokemon['image'] = this.getPokeImage(pokemon['id']);
+        pokemon['pokeIndex'] = pokemon['name'];
+        return pokemon;
+      })
+    );
+  }
+
+  //Gets the full details on individual pokemon
+  getPokeDetails(index){
+    return this.http.get(`${this.baseUrl}/pokemon/${index}`).pipe(
+      map(poke =>{
+      //   let sprites = Object.keys(poke['sprites']);
+      //   poke['images'] = sprites
+      //   .map(spriteKey => poke ['sprites'] [spriteKey])
+      //   .filter(img => img);
+        return poke;
+      })
+    );
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { PokemonService } from '../services/pokemon.service';
 
 @Component({
@@ -9,17 +10,46 @@ import { PokemonService } from '../services/pokemon.service';
 export class Tab3Page implements OnInit {
   offset = 0;
   pokemon = [];
+  @ViewChild(IonInfiniteScroll) infinte: IonInfiniteScroll;
 
   constructor(private pokeService:PokemonService) {}
 
   ngOnInit(){
     this.loadPokemon();
   }
-  loadPokemon(){
+  loadPokemon(loadMore = false, event?){
+    if(loadMore){
+      this.offset += 25;
+    }
     this.pokeService.getPokemon(this.offset).subscribe(result => {
       console.log('result: ', result);
-      this.pokemon = result;
-    })
+      this.pokemon = [...this.pokemon,...result];
+
+      if(event){
+        event.target.complete();
+      }
+      //Set to 125 as 25 is already loaded and theres only 150 pokemon in Gen 1
+      //infinite scroll disabled after 150 loaded as nothing left to load 
+      if(this.offset == 125){
+        this.infinte.disabled = true;
+      }
+    });
   }
 
+  //Search for specific Pokemon from ion searchbar
+  onSearchChange(e){
+    let value = e.detail.value;
+
+    if(value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(result =>{
+      this.pokemon = [result];
+    }, err => {
+      this.pokemon = [];
+    });
+  }
 }
